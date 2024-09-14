9645f8fadb7bf5f3cda567de89a34e2d2d5d7d8a
Nonab1 <abdulazizbekcode@gmail.com>
2024-09-14 20:17:30 +0500

    Intruction

---
+++ weather-cli/index.js
@@ -0,0 +1,16 @@
+const getArgs = requare("/helpers/args")
+
+const startCli = ()=>{
+    const args = getArgs(process.args)
+    if(args.h){
+
+    }
+    if(args.s){
+
+    }
+    if(args.t){
+        
+    }
+}
+
+startCli()
